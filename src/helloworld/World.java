1cc248051d6c45ddf0f295ca7a2ff73a78b27b0c
Sayali <baptesayali123@gmail.com>
2017-12-22 11:24:47 +0530

    Delete charlie branch

--- src/helloworld/World.java
+++ src/helloworld/World.java
@@ -5,6 +5,8 @@ public class World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world Alpha");
+		System.out.println("Hello world beta");
+		System.out.println("Hello world charlie");
 
 	}
 

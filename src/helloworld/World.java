d6703dec767eec5b6f3a2a6ae4d9ea67b47cdb43
Sayali <baptesayali123@gmail.com>
2017-12-22 11:41:00 +0530

    Verify

--- src/helloworld/World.java
+++ src/helloworld/World.java
@@ -5,7 +5,7 @@ public class World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world Alpha");
-		System.out.println("Hello world beta");
+		System.out.println("Hello world Beta");
 		System.out.println("Hello world Charlie");
 
 	}
